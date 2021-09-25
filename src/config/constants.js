aaab0c855957f9e03dce7799b9a3255fec409d30
Jean Diego <jeandiego.ds@gmail.com>
2021-09-25 08:05:59 -0300

    ✨ Added a constants file

---
+++ src/config/constants.js
@@ -0,0 +1,8 @@
+import {
+  getBottomSpace,
+  getStatusBarHeight,
+} from 'react-native-iphone-x-helper';
+
+export const IPHONE_X_HEIGHT = getStatusBarHeight();
+
+export const IPHONE_X_BOTTOM_SPACE = getBottomSpace();
