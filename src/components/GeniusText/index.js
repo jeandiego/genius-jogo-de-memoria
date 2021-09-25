53590b66837400562494a6ab154004a1797eed18
Jean Diego <jeandiego.ds@gmail.com>
2021-09-25 14:32:43 -0300

    💄 Styles improvements on text component

--- src/components/GeniusText/index.js
+++ src/components/GeniusText/index.js
@@ -3,15 +3,15 @@ import styled from 'styled-components/native';
 import { fonts } from '~/global/styles/fonts';
 
 export const Text = styled.Text`
-  font-family: ${(p) => p.fontFamily || 'JosefinSans-Regular'};
   font-family: ${(p) => fonts[p.fontFamily] || 'JosefinSans-Regular'};
   font-size: ${(p) => p.size || 16}px;
-  color: ${(p) => p.theme.colors[p.color] || p.theme.colors.primary};
+  color: ${(p) => p.theme.colors[p.color] || p.theme.colors.textHeading};
   padding-left: ${(p) => p.pLeft || 0}px;
   padding-right: ${(p) => p.pRight || 0}px;
   margin-right: ${(p) => p.mRight || 0}px;
   margin-left: ${(p) => p.mLeft || 0}px;
   padding-bottom: ${(p) => p.pBottom || 0}px;
+  padding-top: ${(p) => p.pTop || 0}px;
   text-align: ${(p) => p.align || 'left'};
 `;
 
