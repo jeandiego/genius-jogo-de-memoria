afe4ac26d2d8d86929313ad38aa3c1c4b58d642f
Jean Diego <jeandiego.ds@gmail.com>
2021-09-25 19:41:59 -0300

    ✨ Created cards services based on assets

---
+++ src/services/cardsServices.js
@@ -0,0 +1,23 @@
+import Avengers from '~/assets/svgs/Avengers.svg';
+import Batman from '~/assets/svgs/Batman.svg';
+import CaptainAmerica from '~/assets/svgs/Captain-America.svg';
+import DC from '~/assets/svgs/DC.svg';
+import Flash from '~/assets/svgs/Flash.svg';
+import GreenLantern from '~/assets/svgs/Green-Lantern.svg';
+import Hawkman from '~/assets/svgs/Hawkman.svg';
+import Spider from '~/assets/svgs/Spider.svg';
+import Superman from '~/assets/svgs/Superman.svg';
+import WonderWoman from '~/assets/svgs/Wonder-Woman.svg';
+
+export const heroesCard = [
+  Avengers,
+  Batman,
+  CaptainAmerica,
+  DC,
+  Flash,
+  GreenLantern,
+  Hawkman,
+  Spider,
+  Superman,
+  WonderWoman,
+];
