f989c5431e78395f7d66a585320fa1ec62969d81
Jean Diego <jeandiego.ds@gmail.com>
2021-09-25 14:32:23 -0300

    ✨ Created leaderboard card component

---
+++ src/components/LeaderboardCard/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import GeniusText from '../GeniusText';
+
+import { Container, ScoreWrapper, RankingView } from './styles';
+
+const LeaderboardCard = ({ leaderboard, ...props }) => {
+  const { username, position, plays } = leaderboard;
+  console.log(username);
+
+  return (
+    <Container {...props}>
+      <RankingView>
+        <GeniusText color="textHeading" fontFamily="bold">
+          {position}º
+        </GeniusText>
+      </RankingView>
+      <GeniusText color="textHeading" size={18} fontFamily="medium">
+        {username}
+      </GeniusText>
+
+      <ScoreWrapper>
+        <GeniusText color="textHeading" size={20} fontFamily="bold">
+          {plays}
+        </GeniusText>
+        <GeniusText color="textHighlight" fontFamily="bold" size={14}>
+          jogadas
+        </GeniusText>
+      </ScoreWrapper>
+    </Container>
+  );
+};
+
+export default LeaderboardCard;
---
+++ src/components/LeaderboardCard/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components/native';
+import { TouchableOpacity } from 'react-native';
+
+export const Container = styled(TouchableOpacity)`
+  background: ${({ theme }) => theme.colors.primary_light};
+  padding: 12px;
+  margin: 4px 12px;
+  border-radius: 8px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ScoreWrapper = styled.View`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const RankingView = styled.View`
+  background: ${({ theme }) => theme.colors.primary_dark};
+  padding: 16px 12px 12px 14px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 6px;
+  border-top-left-radius: 18px;
+  border-bottom-right-radius: 18px;
+`;
