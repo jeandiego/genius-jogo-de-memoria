07fc498e11216ab2dac0e78fa8f7da0d569b5791
Jean Diego <jeandiego.ds@gmail.com>
2021-09-24 22:35:39 -0300

    🎉 Project started

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Ola, mundo</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
