1e0f09f9d7157b7400688022d592e37e2295dce1
Jean Diego <jeandiego.ds@gmail.com>
2021-09-25 14:31:53 -0300

    💄 Adjusted input component style

--- src/components/GeniusInput/styles.js
+++ src/components/GeniusInput/styles.js
@@ -1,18 +1,20 @@
 import styled from 'styled-components/native';
 import { TextInput, Platform } from 'react-native';
+import { fonts } from '~/global/styles/fonts';
 
 export const Container = styled(TextInput).attrs((p) => ({
   padding: Platform.OS === 'ios' ? 15 : 10,
-  placeholderTextColor: p.theme.colors.primary,
+  placeholderTextColor: p.theme.colors.primary_dark,
   returnKeyType: 'send',
   textAlign: 'center',
 }))`
   width: 100%;
   font-size: 18px;
+  font-family: ${(p) => fonts[p.fontFamily] || 'JosefinSans-Regular'};
   border-radius: 8px;
   border-color: ${(p) =>
-    p.isFocused ? p.theme.colors.primary_light : p.theme.colors.primary_dark};
+    p.isFocused ? p.theme.colors.primary_dark : p.theme.colors.primary_light};
   border-width: 2px;
-  background-color: ${({ theme }) => theme.colors.primary_dark};
+  background-color: ${({ theme }) => theme.colors.primary_light};
   color: ${({ theme }) => theme.colors.white};
 `;
