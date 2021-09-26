b97f673a66399d563d7a2b75f9be69a58abdbaa2
Jean Diego <jeandiego.ds@gmail.com>
2021-09-26 18:23:01 -0300

    🐛 Fixed index as id

--- src/utils/cards.js
+++ src/utils/cards.js
@@ -1,7 +1,12 @@
 export function buildCard(cards) {
-  return cards.map((item, index) => ({
-    image: item,
-    status: 'hidden',
-    id: index,
+  return cards.map((item) => ({
+    image: Object.values(item)[0],
+    name: Object.keys(item)[0],
+    id: createCardId(Object.keys(item)[0]),
+    shown: false,
   }));
+}
+
+export function createCardId(card) {
+  return card + parseInt(Math.random() * 1000, 10);
 }
